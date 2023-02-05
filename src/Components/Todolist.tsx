ec3b994674cd7d309cdac17762096bb65115535c
YTim <yun2193@hanmail.net>
2023-02-05 22:10:42 +0900

    카테고리 추가 기능 구현 및 디자인 작업

--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -4,6 +4,8 @@ import { useRecoilState } from "recoil";
 import { todoState } from "../atoms";
 import DroppableCategory from "./DroppableCategory";
 import DroppableRemove from "./DroppableRemove";
+import { useForm } from "react-hook-form";
+import stydles from "../css/Style.module.css";
 
 const Wrapper = styled.div`
   display: flex;
@@ -28,27 +30,67 @@ const AddListDiv = styled.div`
   background-color: #ebecf0;
   border-radius: 5px;
   cursor: pointer;
-  height: 40px;
+  height: fit-content;
   display: flex;
   align-items: center;
-  padding: 4px;
+`;
+const Span = styled.span`
+  width: 100%;
+  padding: 14px;
+  color: #6c7990;
   span {
-    padding: 6px 8px;
-    color: #6c7990;
-    span {
-      padding: 0;
-    }
+    padding: 0;
   }
+`;
+
+const AddListForm = styled.form`
+  width: 100%;
+  background-color: #ebecf0;
+  border-radius: 5px;
+  padding: 4px;
+`;
+
+const InputText = styled.input`
+  padding: 8px 12px;
+  width: 100%;
+  height: 40px;
+  margin-bottom: 5px;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  box-shadow: inset 0 0 0 2px #0079bf;
+  color: rgba(0, 0, 0, 0.8);
+`;
+
+const BtnDiv = styled.div`
+  display: flex;
+`;
+
+const SaveBtn = styled.button`
+  padding: 10px;
+  background-color: #0079bf;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  border-radius: 4px;
+  margin-right: 10px;
+`;
+
+const XButton = styled.button`
+  background-color: transparent;
+  border: none;
+  color: #42526e;
+  cursor: pointer;
+  border-radius: 4px;
+  padding: 0 8px;
   :hover {
     background-color: #dadbe2;
-    span {
-      color: #172b4d;
-    }
   }
 `;
 
 function Todolist() {
   const [todos, setTodos] = useRecoilState(todoState);
+  const { register, setValue, handleSubmit } = useForm();
   const onDragEnd = (info: DropResult) => {
     const { destination, source } = info;
     if (!destination) return;
@@ -93,6 +135,28 @@ function Todolist() {
       });
     }
   };
+  const appearForm = () => {
+    const span = document.querySelector(".addList-span") as HTMLSpanElement;
+    const form = document.querySelector(".addList-form") as HTMLFormElement;
+    span.classList.toggle(stydles.hide);
+    form.classList.toggle(stydles.hide);
+  };
+  const disappearForm = () => {
+    const span = document.querySelector(".addList-span") as HTMLSpanElement;
+    const form = document.querySelector(".addList-form") as HTMLFormElement;
+    span.classList.toggle(stydles.hide);
+    form.classList.toggle(stydles.hide);
+  };
+  const addCategory = ({ list }: any) => {
+    /* 카테고리 추가 생성 코드 */
+    setTodos((oldTodos) => {
+      return {
+        ...oldTodos,
+        [list]: [],
+      };
+    });
+    setValue("list", "");
+  };
   return (
     <>
       <DragDropContext onDragEnd={onDragEnd}>
@@ -107,12 +171,28 @@ function Todolist() {
                 />
               ))}
               <AddListDiv>
-                <span>
+                <Span onClick={appearForm} className="addList-span">
                   <span style={{ marginRight: "5px" }}>
                     <i className="fa-solid fa-plus"></i>
                   </span>
                   Add another list
-                </span>
+                </Span>
+                <AddListForm
+                  onSubmit={handleSubmit(addCategory)}
+                  className={stydles.hide + " addList-form"}
+                >
+                  <InputText
+                    {...register("list", { required: true })}
+                    type="text"
+                    placeholder="Enter list title..."
+                  />
+                  <BtnDiv>
+                    <SaveBtn type="submit">Add List</SaveBtn>
+                    <XButton type="button" onClick={disappearForm}>
+                      <i className="fa-solid fa-xmark fa-2x"></i>
+                    </XButton>
+                  </BtnDiv>
+                </AddListForm>
               </AddListDiv>
             </Boards>
             <DroppableRemove />
